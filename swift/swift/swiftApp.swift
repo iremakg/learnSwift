bb774291c97687823285b16af416efb65e2241a3
iremakg <100313534+iremakg@users.noreply.github.com>
2023-04-27 21:58:39 +0300

    Add files via upload

---
+++ swift/swift/swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftApp.swift
+//  swift
+//
+//  Created by İrem Akgoz on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
